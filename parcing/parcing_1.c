e906ce4281a43e3148c5437fd94c58a556a315b9
Mohamed Louazir <mlouazir@e3r4p15.1337.ma>
2024-04-30 17:12:03 +0100

    p

--- parcing/parcing_1.c
+++ parcing/parcing_1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parcing_1.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amajid <amajid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mlouazir <mlouazir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 16:16:55 by mlouazir          #+#    #+#             */
-/*   Updated: 2024/04/30 17:06:45 by amajid           ###   ########.fr       */
+/*   Updated: 2024/04/30 17:08:11 by mlouazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int map_name_check(char *filename)
 {
 	const long size = ft_strlen(filename);
-	const ex_size = ft_strlen(".cub");
+	const int ex_size = ft_strlen(".cub");
 	if(size <= ex_size)
 		return 0;
 	if(ft_strncmp(filename - ex_size, ".cub", ex_size) != 0)
