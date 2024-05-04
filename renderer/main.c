c47ac906bedbd3b50136fb77072435a8d6046362
amajid <abdelilahmajid012@gmail.com>
2024-05-04 17:45:49 +0100

    adding input

--- renderer/main.c
+++ renderer/main.c
@@ -1,9 +1,13 @@
 #include <mlx.h>
+#include <stdio.h>
+#include <math.h>
 
 #define mapWidth 24
 #define mapHeight 24
 #define screenWidth 1920
 #define screenHeight 1080
+
+
 
 int worldMap[mapWidth][mapHeight]=
 {
@@ -41,6 +45,18 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct s_vars
+{
+	double posX, posY;  //x and y start position
+	double dirX, dirY; //initial direction vector
+	double planeX, planeY; //the 2d raycaster version of camera plane
+	unsigned int time; //time of current frame
+	//speed modifiers
+	double moveSpeed; //the constant value is in squares/second
+	double rotSpeed; //the constant value is in radians/second
+
+} t_vars;
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -69,21 +85,67 @@ double abs_d(double x)
 	return x;
 }
 
+int	key_hook(int keycode, t_vars *v)
+{
+	printf("Hello from key_hook!, %d\n", keycode);
+
+	  //move forward if no wall in front of you
+    if (keycode == 13)
+    {
+      if(worldMap[(int)(v->posX + v->dirX * v->moveSpeed)][(int)(v->posY)] == 0) v->posX += v->dirX * v->moveSpeed;
+      if(worldMap[(int)(v->posX)][(int)(v->posY + v->dirY * v->moveSpeed)] == 0) v->posY += v->dirY * v->moveSpeed;
+    }
+    //move backwards if no wall behind you
+    if (keycode == 1)
+    {
+      if(worldMap[(int)(v->posX - v->dirX * v->moveSpeed)][(int)(v->posY)] == 0) v->posX -= v->dirX * v->moveSpeed;
+      if(worldMap[(int)(v->posX)][(int)(v->posY - v->dirY * v->moveSpeed)] == 0) v->posY -= v->dirY * v->moveSpeed;
+    }
+    //rotate to the right
+    if (keycode == 124)
+    {
+      //both camera direction and camera plane must be rotated
+      double oldDirX = v->dirX;
+      v->dirX = v->dirX * cos(-v->rotSpeed) - v->dirY * sin(-v->rotSpeed);
+      v->dirY = oldDirX * sin(-v->rotSpeed) + v->dirY * cos(-v->rotSpeed);
+      double oldPlaneX = v->planeX;
+      v->planeX = v->planeX * cos(-v->rotSpeed) - v->planeY * sin(-v->rotSpeed);
+      v->planeY = oldPlaneX * sin(-v->rotSpeed) + v->planeY * cos(-v->rotSpeed);
+    }
+    //rotate to the left
+    if (keycode == 123)
+    {
+      //both camera direction and camera plane must be rotated
+      double oldDirX = v->dirX;
+      v->dirX = v->dirX * cos(v->rotSpeed) - v->dirY * sin(v->rotSpeed);
+      v->dirY = oldDirX * sin(v->rotSpeed) + v->dirY * cos(v->rotSpeed);
+      double oldPlaneX = v->planeX;
+      v->planeX = v->planeX * cos(v->rotSpeed) - v->planeY * sin(v->rotSpeed);
+      v->planeY = oldPlaneX * sin(v->rotSpeed) + v->planeY * cos(v->rotSpeed);
+    }
+
+	return (0);
+}
+
 int main()
 {
 
 	t_data img;
-	double posX = 22, posY = 12;  //x and y start position
-	double dirX = -1, dirY = 0; //initial direction vector
-	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
-
-	// double time = 0; //time of current frame
-	// double oldTime = 0; //time of previous frame
+	t_vars v = {.posX = 22, .posY = 12,  //x and y start position
+		.dirX = -1, .dirY = 0, //initial direction vector
+		.planeX = 0, .planeY = 0.66, //the 2d raycaster version of camera plane
+		.time = 0, //time of current frame
+		.moveSpeed = 0, //the constant value is in squares/second
+		.rotSpeed = 0, //the constant value is in radians/second
+	};
+	
+	
 	void	*mlx;
 	void	*mlx_win;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, screenWidth, screenHeight, "cube3d");
+	mlx_key_hook(mlx_win, key_hook, &v);
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 	&img.endian);
@@ -92,12 +154,12 @@ int main()
 	{
 		//calculate ray position and direction
 		double cameraX = 2 * x / (double)w - 1; //x-coordinate in camera space
-		double rayDirX = dirX + planeX * cameraX;
-		double rayDirY = dirY + planeY * cameraX;
+		double rayDirX = v.dirX + v.planeX * cameraX;
+		double rayDirY = v.dirY + v.planeY * cameraX;
 
 		//which box of the map we're in
-		int mapX = (int)posX;
-		int mapY = (int)posY;
+		int mapX = (int)v.posX;
+		int mapY = (int)v.posY;
 
 		//length of ray from current position to next x or y-side
 		double sideDistX;
@@ -118,22 +180,22 @@ int main()
 		if (rayDirX < 0)
 		{
 			stepX = -1;
-			sideDistX = (posX - mapX) * deltaDistX;
+			sideDistX = (v.posX - mapX) * deltaDistX;
 		}
 		else
 		{
 			stepX = 1;
-			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+			sideDistX = (mapX + 1.0 - v.posX) * deltaDistX;
 		}
 		if (rayDirY < 0)
 		{
 			stepY = -1;
-			sideDistY = (posY - mapY) * deltaDistY;
+			sideDistY = (v.posY - mapY) * deltaDistY;
 		}
 		else
 		{
 			stepY = 1;
-			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+			sideDistY = (mapY + 1.0 - v.posY) * deltaDistY;
 		}
 
 		//perform DDA
@@ -170,13 +232,12 @@ int main()
 		if(drawEnd >= h)drawEnd = h - 1;	
 		//choose wall color
 		int color;
-		switch(worldMap[mapX][mapY])
 		{
-			case 1:  color = 0x00FF0000;  break; //red
-			case 2:  color = 0x0000FF00;  break; //green
-			case 3:  color = 0x000000FF;   break; //blue
-			case 4:  color = 0x00FFFFFF;  break; //white
-			default: color = 0x00FFFF00; break; //yellow
+			color = 0x00FF0000 * (worldMap[mapX][mapY] == 1) +  //red
+			0x0000FF00 * (worldMap[mapX][mapY] == 2) +   //green
+			0x000000FF * (worldMap[mapX][mapY] == 3) +  //blue
+			0x00FFFFFF * (worldMap[mapX][mapY] == 4) +  //white
+			0x00FFFF00 * (worldMap[mapX][mapY] == 0	);  //yellow
 		}
 
 		//give x and y sides different brightness
@@ -184,6 +245,12 @@ int main()
 
 		//draw the pixels of the stripe as a vertical line
 		draw_verline(&img, x, drawStart, drawEnd, color);
+		//timing for input and FPS counter
+		v.time++;
+
+		//speed modifiers
+		v.moveSpeed = 5.0; //the constant value is in squares/second
+		v.rotSpeed =  3.0; //the constant value is in radians/second
 	}
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
